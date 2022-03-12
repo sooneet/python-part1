7e11630b3ac9b957ad5b0f7a92311f6aa4a43134
mesooneet <mesooneet@gmail.com>
2022-03-12 07:24:33 -0800

    fourth comment

--- strings.py
+++ strings.py
@@ -22,3 +22,64 @@ print('expensive' not in txt)
 txt = "The best things in life are free!"
 if 'expensive' not in txt:
     print("Yes, 'expensive' is not present.")
+
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[2:])
+
+b = "Hello, World!"
+print(b[-6:-3])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip())
+
+a = "Hello, World!"
+print(a.replace('H','J'))
+
+a = "Hello, World!"
+b = a.split(',')
+print(b)
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 35
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity,itemno,price))
+
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {2} pieces of item {3} for {1} dollars."
+print(myorder.format(quantity,itemno,price))
+
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt) 
