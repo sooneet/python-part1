317ac74a76d0adf97600b1c9996d43f5d14b2fff
mesooneet <mesooneet@gmail.com>
2022-03-12 07:09:11 -0800

    third comment

---
+++ numbers.py
@@ -0,0 +1,29 @@
+x = 5
+y = 5.5
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#convert from int to float:
+x = float(1)
+
+#convert from float to int:
+y = int(2.8)
+
+#convert from int to complex:
+z= complex(1)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+import random
+print(random.randrange(1,10))
---
+++ strings.py
@@ -0,0 +1,24 @@
+a = 'welcome'
+print(a[0])
+
+for x in 'banana':
+  print(x)
+
+a = "Hello"
+print(len(a))
+
+txt = "The best things in life are free!"
+print('free' in txt)
+
+
+txt = "The best things in life are free!"
+if 'free' in txt:
+    print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print('expensive' not in txt)    
+
+
+txt = "The best things in life are free!"
+if 'expensive' not in txt:
+    print("Yes, 'expensive' is not present.")
